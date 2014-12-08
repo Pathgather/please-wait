ed35754c4a60b5c5b8515041ba55f3f6baa3f738
Jamie Davidson <jhdavids8@gmail.com>
2014-12-07 19:16:31 -0500

    Progress on demo page

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,6 +1,8 @@
 angular.module('pleaseWaitApp', [])
   .controller('MainCtrl', ['$scope', function($scope) {
-    $scope.please_wait_options = {};
+    $scope.please_wait_options = {
+      backgroundColor: '#f46d3b'
+    };
 
     $scope.updatePleaseWait = function() {
 
